3130c3fd23d22de0a9968be6489dcf067cfe9166
Alexandre <alex.desvallees@gmail.com>
2019-04-26 12:49:47 +0200

    màj du bat

--- scripts/batch-image-resize.bat
+++ scripts/batch-image-resize.bat
@@ -11,23 +11,17 @@ REM ----- .png, .jpeg, other ? -----
 REM GATA Project specific resize !
 
 REM Absolute path to image folder
-FOLDER="/media/alexandre/8628ADC128ADB11B/Cours/Machine_Learning/ML-Projet-GATA/dataset"
 
 REM Max height
-WIDTH=300
+set WIDTH=300
 
 REM Max width
-HEIGHT=400
+set HEIGHT=400
 
-# Par défaut, on va utiliser des images en .jpg
-# Resize png or jpg to either height or width, keeps proportions using imagemagick
-#find ${FOLDER} -iname '*.jpg' -o -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+@echo off
+set "FOLDER=c:\images"
+set "result_folder_1=c:\images\resized"
 
-# Resize png to either height or width, keeps proportions using imagemagick
-#find ${FOLDER} -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
-
-# Resize jpg only to either height or width, keeps proportions using imagemagick
-find ${FOLDER} -iname '*.jpg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
-
-# Alternative
-#mogrify -path ${FOLDER} -resize ${WIDTH}x${HEIGHT}% *.png -verbose
+for %%a in ("%FOLDER%\*jpg") do (
+   call scale.bat -source "%%~fa" -target "%result_folder_1%\%%~nxa" -max-height HEIGHT -max-width WIDTH -keep-ratio no -force yes
+)
