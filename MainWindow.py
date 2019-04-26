fa167f4859aeefffb9db2f3ec43385ff74a79615
Rock3f <badier.tanguy@gmail.com>
2019-04-26 10:32:38 +0200

    Add method Reconnaissande image

--- MainWindow.py
+++ MainWindow.py
@@ -5,8 +5,67 @@ import cv2
 import os
 import numpy as np
 
-def getRGB(source):
+def get_RGB(source):
     return cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
+
+def reconnaissance_image(encodings, image, detection_method): 
+	#Chargement du fichier .pickle (dataset)
+	print("Chargement du dataset Dump")
+	data = pickle.loads(open(encodings, "rb").read())
+
+	# load the input image and convert it from BGR to RGB
+	image = cv2.imread(image)
+	rgb = get_RGB(image)	
+
+	# Detection des coordonnées (x,y) de chaque visage sur l'image 
+	print("Reconnaissances des visages")
+	boxes = face_recognition.face_locations(rgb, model=detection_method)
+	#Encodage des visages
+	encodings = face_recognition.face_encodings(rgb, boxes)
+
+	# Initialisation du tableau de variable
+	names = []
+
+	# Boucle sur l'ensemble des visages détectés 
+	for encoding in encodings:
+		# Comparaison des visages
+		matches = face_recognition.compare_faces(encodings,encoding)
+		name = "Non reconnu"
+
+		# Si le visage correspond
+		if True in matches:
+			# Récupération des index de tous les visages qui correspondent
+			# et Création d'un dictionnaire permettant de compter le nombre totale de visages reconnus 
+			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+			counts = {}
+
+			# Boucle sur l'ensemble des index correspondant et mise à jour d'un compteur par nom
+			for i in matchedIdxs:
+				name = data["names"][i]
+				counts[name] = counts.get(name, 0) + 1
+
+			# Récupération du nom dont le compteur est la plus grande valeur
+			#Il s'agit ici d'avoir le nom dont la probabilité est la plus grande
+			name = max(counts, key=counts.get)
+		
+		# Mise à jour de la liste de noms
+		names.append(name)
+
+	# Boucle sur l'ensemble des visage reconnu
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+		# Dessine un rectangle et le nom sur le visage reconnu
+		cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
+		y = top - 15 if top - 15 > 15 else top + 15
+		cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			0.75, (0, 255, 0), 2)
+
+	print("Affichage de l'image")
+	print ("<-- Veuillez changer de fenêtre -->")
+	# show the output image
+	cv2.imshow("Image", image)
+
+	print("Après avoir vu votre belle image, appuyer sur n'importe quelle touche")
+	cv2.waitKey(0)
 
 def load_model(path_dataset, path_encoding, detection_method ) :
 
@@ -26,7 +85,7 @@ def load_model(path_dataset, path_encoding, detection_method ) :
 
 		# Chargment des images et conversion en RGB (OpenCV ordering)
 		image = cv2.imread(image_path)
-		rgb = getRGB(image)
+		rgb = get_RGB(image)
 
 		# Détection de chaque visage par coordonnées(x, y)
 		# correspondant à chaque visage sur une image
@@ -83,7 +142,7 @@ def main():
 	path_dataset = "dataset"
 	path_encoding = "encodings.pickle"
 	detection_method = "cnn"
-	menuItems = np.array(["Charger le modèle de données", "Quitter"])
+	menuItems = np.array(["Charger le modèle de données", "Reconnaissance de l'image", "Quitter"])
 	print()
 	print("------------------------------------------------")
 	print()
@@ -96,6 +155,10 @@ def main():
 		if choice == 1:
 			load_model(path_dataset,path_encoding,detection_method)
 		elif choice == 2:
+			path_image = input("Saisissez l'URI de l'image de test : ")
+
+			reconnaissance_image(path_encoding, path_image, detection_method)
+		elif choice == 3:
 			print("Fermeture de l'application...")
 			break
 
